0f4dfb0a7d4c1b1b0ece14ae5e5ddefcf585a29f
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-09-19 18:03:59 +0200

    Learning git

--- Program.cs
+++ Program.cs
@@ -2,7 +2,21 @@
 
 class Program
 {
-    static void Main(string[] args) {
-        Console.WriteLine("Hello, World!");
+    static void Main(string[] args)
+    {
+        int[] myArray = new int[5];
+        try
+        {
+            Console.WriteLine(myArray[4]);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+        finally
+        {
+            Console.WriteLine("End");
+        }
+        
     }
 }

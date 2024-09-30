ccb81f73e1dd12691088e31dc5487496eefdcd10
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-09-30 14:20:04 +0200

    Reset files

--- Person.cs
+++ Person.cs
@@ -2,9 +2,5 @@
 
 public class Person
 {
-    
-    public static int _number = 5;
-
-    public int _fooNumber;
-
+            
 }
--- Program.cs
+++ Program.cs
@@ -7,21 +7,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        string[] values = { "12,3", "45", "ABC", "11", "DEF" };
-
         
-        decimal sum = 0;
-        string letters = "";
-
-        foreach (string value in values)
-        {
-            if (decimal.TryParse(value, out decimal result))
-            {
-                sum += result;
-            }
-            else letters += value;
-        }
-        Console.WriteLine(sum);
-        Console.WriteLine(letters);
     }
+
+
 }

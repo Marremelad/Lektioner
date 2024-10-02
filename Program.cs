0c7178eb5296e3f7c9390ba21ad10824250e4f03
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-10-02 09:18:17 +0200

    Remove using statements

--- Program.cs
+++ Program.cs
@@ -1,18 +1,11 @@
-﻿using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices.Marshalling;
-using System.Text.Json.Serialization;
-
+﻿
 namespace Lektioner;
 
 class Program
 {
     public static void Main(string[] args)
     {
-        Vehicle vehicle = new Vehicle("KEK", "W", 1999);
-        Vehicle kek = new Vehicle("hej", "dig", 2001, "bs");
         
-        vehicle.Display();
-        kek.Display();
     }
     
 }

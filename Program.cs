c943dbb3ab7be6d0991c5e2c07a4061186e19b1b
Rasemouse <rasmuswenngren@gmail.com>
2024-10-11 15:39:37 +0200

    Changed code in Program.cs added .gitignore

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿ls
+using System.Diagnostics;
 
 namespace Lektioner;
 
@@ -6,50 +7,7 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        List<int> myNumbers = [1, 2, 3];
-        List<string> myStrings = ["Mauricio", "Tobias", "Leo"];
-        List<object> myObjects = ["Jake", 1, 2, 3];
-
-        Stopwatch sw = new Stopwatch();
-        sw.Start();
-
-        for (int i = 0; i < 1000000; i++)
-        {
-            myObjects.Add(i);
-        }
-        sw.Stop();
-        Console.WriteLine($"List of objects took: {sw.ElapsedMilliseconds} milliseconds.");
-
-        sw = new Stopwatch();
-        sw.Start();
-        
-        for (int i = 0; i < 1000000; i++)
-        {
-            myNumbers.Add(i);
-        }
-        sw.Stop();
-        Console.WriteLine($"List of numbers took: {sw.ElapsedMilliseconds} milliseconds.");
-
-        sw = new Stopwatch();
-        sw.Start();
-        
-        for (int i = 0; i < 1000000; i++)
-        {
-            myStrings.Add($"{i}");
-        }
-        sw.Stop();
-        Console.WriteLine($"List of strings took: {sw.ElapsedMilliseconds} milliseconds.");
-        
-        
-        sw = new Stopwatch();
-        sw.Start();
-        
-        for (int i = 0; i < 1000000; i++)
-        {
-            myObjects.Add($"{i}");
-        }
-        sw.Stop();
-        Console.WriteLine($"List of strings in object list took: {sw.ElapsedMilliseconds} milliseconds.");
+        Console.WriteLine("Hej! Här tagait bort din kod :)"); 
     }
 }
 

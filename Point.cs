17b86ffc8f8805e1e33f8b62ee5e70cb26db6687
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-10-14 13:14:33 +0200

    Clean up file

--- Point.cs
+++ Point.cs
@@ -1,6 +1,15 @@
-﻿namespace Lektioner;
+﻿using System.ComponentModel.Design;
+
+namespace Lektioner;
 
 public struct Point
 {
-    
+    public int X;
+    public int Y;
+
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
 }

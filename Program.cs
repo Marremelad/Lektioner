d458792ad8f365937eaf756cfbeced00c8d5321c
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-10-16 21:24:16 +0200

    Practicing with lambdas and delegates

--- Program.cs
+++ Program.cs
@@ -1,10 +1,25 @@
-﻿namespace Lektioner;
+﻿using System.Threading;
+using Microsoft.VisualBasic.CompilerServices;
 
-public class Program
+namespace Lektioner;
+
+using System;
+
+class Program
 {
     public static void Main(string[] args)
     {
-        
-    }
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        var evenNumbers = numbers.Where(n => n % 2 == 0);
+
+        foreach (var num in evenNumbers)
+        {
+            Console.WriteLine(num);
+        }
+
+    }    
+    
 }
+
 

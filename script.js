88f66d9ebfa9e47d1290343070b37ffbec19b13a
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-02-06 19:50:35 +0100

    Rick and morty character generator

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
-const element = document.querySelector("#paragraph-id")
+// https://rickandmortyapi.com/api/character
+const characterPicture = document.querySelector("#character-picture");
+const characterName = document.querySelector("#character-name");
+const generateCharacterButton = document.querySelector("#generate-character-button")
 
-element.classList.add("some-class");
+function displayCharacterInfo(character) {
+    characterPicture.src = character.image;
+    characterName.textContent = character.name
+}
 
+function getRandomCharacter(data) {
+    const character = data.results[Math.floor(Math.random() * data.results.length)];
+    displayCharacterInfo(character);
+}
+
+async function getRickAndMortyCharacter() {
+    try {
+        const response = await fetch("https://rickandmortyapi.com/api/character");
+        const data = await response.json();
+        getRandomCharacter(data);
+    } catch (e) {
+        console.log(e);
+        console.log("Something went wrong :(");
+    }
+}
+
+generateCharacterButton.addEventListener("click", () => {
+    getRickAndMortyCharacter()
+})
